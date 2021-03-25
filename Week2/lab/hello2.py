4f8a592c39cf293607f9e05bd301b02d6dcae6f9
Amy <G00235358@gmit.ie>
2021-03-25 16:58:29 +0000

    Lab 2.2 - First Programs_Push05

--- Week2/lab/hello2.py
+++ Week2/lab/hello2.py
@@ -1,3 +1,7 @@
+# hello2.py 
+# display a personalised greeting
+# Author: Amy Reynolds (Andrew Beatty - lab 2.2 - first programs)
+
 name = input ("enter you name:")
 print ("Hello " + name)
 # Using \t to create tab separation for greeting 
@@ -7,3 +11,12 @@ print ("Hello "+ name +"\t nice to meet you ")
 # Using \n to create new line for greeting 
 print ("Hello \nNice to meet you " + name)
 print ("Hello " + name + "\nNice to meet you ")
+
+# format function
+print ("Hello {} ".format (name))
+
+# format function - tab separation for greeting 
+print ("Hello {} \tNice to meet you " . format (name))
+
+# format fucntion - new line separation for greeting
+print ("Hello {} \nNice to meet you " .format (name))
--- Week2/lab/multiply.py
+++ Week2/lab/multiply.py
@@ -1,2 +1,6 @@
+# multiply.py 
+# display output calculation
+# Author: Amy Reynolds (Andrew Beatty - lab 2.2 - first programs)
+
 print (111 * 555)
 
