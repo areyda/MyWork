e833cd9e6b9855f5839a4853fd36ec17605be3eb
Amy <G00235358@gmit.ie>
2021-03-25 16:47:41 +0000

    Lab 2.2 - First Programs_Push04

--- Week2/lab/hello2.py
+++ Week2/lab/hello2.py
@@ -1,2 +1,9 @@
 name = input ("enter you name:")
 print ("Hello " + name)
+# Using \t to create tab separation for greeting 
+print ("Hello \t nice to meet you " + name)
+print ("Hello "+ name +"\t nice to meet you ")
+
+# Using \n to create new line for greeting 
+print ("Hello \nNice to meet you " + name)
+print ("Hello " + name + "\nNice to meet you ")
