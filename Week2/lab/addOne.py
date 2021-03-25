3783b6b244cc030c056724e5c730f462ce7dac0a
Amy <G00235358@gmit.ie>
2021-03-25 17:10:49 +0000

    Lab 2.2 - First Programs_Push06

---
+++ Week2/lab/addOne.py
@@ -0,0 +1,11 @@
+# addOne.py 
+# Increments the entered value by +1
+# Author: Amy Reynolds (Andrew Beatty - lab 2.2 - first programs)
+
+
+number = int(input ("please enter a number:"))
+number2 = number + 1
+print (number2)
+
+# Using format function instead of int function for str vs int
+print (" {} plus one is {} " .format (number, number2))
